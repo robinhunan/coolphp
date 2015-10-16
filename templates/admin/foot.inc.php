690f0f2027cf633a89a6790802e703eb9d095705
Your Name <yubing@yubingtekiAir.lan>
2015-10-16 10:51:48 +0800

    add admin inc

---
+++ templates/admin/foot.inc.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="static/js/common.js"></script>
+<script type="text/javascript" src="static/js/admin.js"></script>
+</body>
+</html>
---
+++ templates/admin/head.inc.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<meta charset="UTF-8">
+<title>列表页</title>
+<head>
+<link rel="stylesheet" type="text/css" href="static/css/base.css" />
+<link rel="stylesheet" type="text/css" href="static/css/admin.css" />
+</head>
+<body>
+
+<!--top-->
+<div class="company-info-top">
+    <div class="companyInfoLogo"><img src="http://mat1.gtimg.com/www/images/qq2012/qqlogo.png" /></div>
+    <ul class="company-list">
+        <li class="files"><a href="#">消息中心</a><span>12</span></li>
+        <li class="files notice"><a href="#">公告</a><span>0</span></li>
+		
+    </ul>
+</div>
+<!-- top end -->
+<div class="C"></div>
+<!--nav-->
+
+<div class="company-info-nav">
+    <div class="info-nav">
+        <ul class="info-nav-list">
+        	
+            <li><a href="#" target="_self" >成交助手</a></li>
+            <li><a href="#" target="_self">网商助手</a></li>
+        </ul>
+        <ul class="login-info">
+                            <li class="hello">
+                <span class="welcome">测试商家</span><span>&nbsp;</span>
+                <span class="hello2"><a class="col" href="#">[退出]</a></span></li>
+                
+            <li class="R">
+                <a href="#">会员中心</a>
+            </li>
+
+        </ul>
+    </div>
+</div>
+<div class="C"></div>
+<!--end top-->
+
+	<!--信息-->
+	<ul class="companyInfo-list">
+		<li><a href="index.html" class="activee">咨询助手</a>></li>
+		<li>欢迎页</li>
+	</ul>
+        
+  
+	<!--内容-->
+	<div class="companyInfo-con">
+
+<!--公共左-->
+		<div class="companyInfo-left">
+	<ul class="companyInfo-con-left">
+        <li class="topMargin"><a href="javascript:void(0)">咨询助手</a></li>
+        		<li class="list-type">
+             <a href="javascript:;" target="_self" class="list-type-b">咨询管理</a>
+             <img alt="" src="/static/images/down.png" />
+             			<ol class="secondary-list" >
+							<li><a href="#" target="_self">微信助手</a></li>
+							<li><a href="#" target="_self">微商助手</a></li>
+							<li><a href="#" target="_self">咨询助手</a></li>
+	            		 </ol>
+	         		</li>
+				<li class="list-type">
+             <a href="javascript:;" target="_self" class="list-type-b">在线咨询统计</a>
+             <img alt="" src="/static/images/down.png">
+             			<ol class="secondary-list" >
+             		            <li ><a href="#">经销商统计</a></li>
+	            			</ol>
+	         		</li>
+		    </ul>
+    
+    <div class="hot-line">
+        <p>统一服务热线</p>
+		<p class="phone-fore" style="font-size: 24px;line-height: 45px;">400-888-8888</p>
+		<p>服务时间:9:00至18:00</p>
+    </div>
+
+    </div>
+    
+<!--公共左  end-->
