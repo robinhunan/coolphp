70fb2fb3a57483b9aabe10eb5a5740cbd41f5f16
Your Name <yubing@yubingtekiAir.lan>
2015-10-16 11:03:36 +0800

    add test data

--- tools/cool.sql
+++ tools/cool.sql
@@ -1,38 +1,85 @@
+-- MySQL dump 10.13  Distrib 5.6.26, for osx10.10 (x86_64)
 --
--- 表的结构 `manager`
---
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.26
 
-CREATE TABLE IF NOT EXISTS `manager` (
-  `id` smallint(6) NOT NULL auto_increment,
-  `userName` varchar(64) NOT NULL COMMENT '用户名',
-  `userPass` varchar(64) NOT NULL COMMENT '密码',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
-
---
--- 导出表中的数据 `manager`
---
-
-INSERT INTO `manager` (`id`, `userName`, `userPass`) VALUES
-(1, 'admin', 'admin');
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- 表的结构 `user`
+-- Table structure for table `app1_user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` smallint(6) NOT NULL auto_increment,
+DROP TABLE IF EXISTS `app1_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app1_user` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `loginName` varchar(20) NOT NULL COMMENT '用户名',
   `loginPass` varchar(32) NOT NULL COMMENT '密码',
   `nickName` varchar(20) NOT NULL COMMENT '昵称',
-  `sex` enum('男','女','保密') NOT NULL default '保密' COMMENT '性别',
+  `sex` enum('男','女','保密') NOT NULL DEFAULT '保密' COMMENT '性别',
   `birthday` int(11) NOT NULL COMMENT '生日',
   `province` varchar(10) NOT NULL COMMENT '省份',
   `city` varchar(64) NOT NULL COMMENT '城市',
   `mobile` bigint(20) unsigned NOT NULL COMMENT '城市',
   `email` varchar(255) NOT NULL COMMENT '注册邮箱',
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `loginName` (`loginName`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app1_user`
+--
+
+LOCK TABLES `app1_user` WRITE;
+/*!40000 ALTER TABLE `app1_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app1_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manager` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(64) NOT NULL COMMENT '用户名',
+  `userPass` varchar(64) NOT NULL COMMENT '密码',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'admin','admin'),(2,'2','2'),(3,'3','3'),(4,'4','4'),(6,'ee','eee');
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-16 11:02:09
